57dc6fce1f2d8f3f706ce12728d8fa08fb0b1cbc
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-08-21 22:19:43 +0200

    created a ListNode class used by LinkedList class

--- ListNode.java
+++ ListNode.java
@@ -1,10 +1,10 @@
 public class ListNode {
-  
+
 // Definition for singly-linked list.
     int val;
     ListNode next;
     ListNode() {
-        
+
     }
     ListNode(int val){
         this.val = val; 
