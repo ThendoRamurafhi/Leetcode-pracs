996982a352f960f97a2916be7206091fe5c2d14e
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-08-21 22:09:38 +0200

    Solving some syntax error

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -36,13 +36,13 @@ public class LongestSubstring {
         Set<Character> seen = new HashSet<>();
         int left = 0, maxLen = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            while (seen.contains(s.charAt(i))) {
+        for (int right = 0; right < s.length(); right++) {
+            while (seen.contains(s.charAt(right))) {
                 seen.remove(s.charAt(left));
                 left++;
             }
-            seen.add(s.charAt(i));
-            maxLen = Math.max(maxLen, i - left + 1);
+            seen.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
         }
 
         return maxLen;
