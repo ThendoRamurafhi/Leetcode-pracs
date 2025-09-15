048f788f3c5f94334b2a999017de7c3d36aa03bb
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-09-16 00:50:20 +0200

    final product of the zigzag file

--- ZigzagConversion.java
+++ ZigzagConversion.java
@@ -7,31 +7,69 @@ public class ZigzagConversion {
         String input = scan.nextLine();
 
         ArrayList<Character> arrlist = new ArrayList<>();
-
-        for(int i = 0; i < input.length(); i++){
-            
+        for (int i = 0; i < input.length(); i++) {
             arrlist.add(input.charAt(i));
         }
 
-        // for (int i = 0; i < arrlist.size(); i++) {
-        //     System.out.print(arrlist.get(i));
-        // }
+        int numRows = 3; // you can change this
+        if (numRows == 1 || numRows >= arrlist.size()) {
+            System.out.println(input);
+            return;
+        }
 
-        char[][] yh = new char[3][arrlist.size()];
-
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < arrlist.size(); j ++ ){
-                yh[i][j] = (char) j;            
+        // create 2D grid, fill with spaces
+        char[][] yh = new char[numRows][arrlist.size()];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < arrlist.size(); j++) {
+                yh[i][j] = ' ';
             }
         }
 
-        for (int i = 0; i < arrlist.size(); i++){
-            for (int j = 0; j < arrlist.size(); j++){
-                System.out.println(yh[i][j]);
+        // zigzag traversal
+        int row = 0;
+        int col = 0;
+        boolean goingDown = true;
+
+        for (char c : arrlist) {
+            yh[row][col] = c;
+
+            if (goingDown) {
+                if (row == numRows - 1) {
+                    goingDown = false;
+                    row--;
+                    col++;
+                } else {
+                    row++;
+                }
+            } else {
+                if (row == 0) {
+                    goingDown = true;
+                    row++;
+                } else {
+                    row--;
+                    col++;
+                }
             }
         }
 
-        int row = 0, col = 0;
-        
+        // print the zigzag grid
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < arrlist.size(); j++) {
+                System.out.print(yh[i][j]);
+            }
+            System.out.println();
+        }
+
+        // also collect result row by row
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < arrlist.size(); j++) {
+                if (yh[i][j] != ' ') {
+                    result.append(yh[i][j]);
+                }
+            }
+        }
+
+        System.out.println("Converted: " + result.toString());
     }
 }
