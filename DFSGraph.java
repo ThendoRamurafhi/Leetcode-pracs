a015fc0fcfc3a31a2f34332f878f11b712883b39
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-09-16 05:09:50 +0200

    it was missing the main method just add it

--- DFSGraph.java
+++ DFSGraph.java
@@ -18,4 +18,39 @@ public class DFSGraph {
             }
         }
     }
+
+     // To add an edge in an undirected graph
+    public static void
+    addEdge(ArrayList<ArrayList<Integer> > adj, int s,
+            int t)
+    {
+        adj.get(s).add(t);
+        adj.get(t).add(s);
+    }
+
+    public static void main(String[] args)
+    {
+        int V = 5;
+        ArrayList<ArrayList<Integer> > adj
+            = new ArrayList<>();
+
+        // Initialize adjacency list
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Add edges
+        int[][] edges= { { 1, 2 },{ 1, 0 },{ 2, 0 },{ 2, 3 },{ 2, 4 } }; 
+        for (int[] e : edges)
+        {
+            addEdge(adj, e[0], e[1]);
+        }
+
+        // Perform DFS starting from vertex 0
+        ArrayList<Integer> res = DFS(adj);
+
+        for (int i = 0; i < res.size(); i++) {
+            System.out.print(res.get(i) + " ");
+        }
+    }
 }
