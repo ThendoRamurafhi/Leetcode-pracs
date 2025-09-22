e76c0d15b40d536513201a6a9c895bb4fc8a2ac3
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-09-22 18:49:00 +0200

    funished version of this file

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,5 @@
 public class BinarySearch {
+    // iterative binary search function
     public static int binarySearch(int[] arr, int lo, int hi, int x){
 
         while(lo<=hi){
@@ -8,5 +9,59 @@ public class BinarySearch {
             else hi = mid - 1;
             
         }return-1;
+    }
+
+    static int findPivot(int[] arr, int lo, int hi) {
+        while (lo <= hi) {
+          
+            // The current subarray is already sorted,
+            // the minimum is at the low index
+            if (arr[lo] <= arr[hi])
+                return lo;
+
+            int mid = (lo + hi) / 2;
+
+            // The right half is not sorted. So
+            // the minimum element must be in the
+            // right half
+            if (arr[mid] > arr[hi])
+                lo = mid + 1;
+
+            // The right half is sorted. Note that in
+            // this case, we do not change high to mid - 1
+            // but keep it to mid. The mid element
+            // itself can be the smallest
+            else
+                hi = mid;
+        }
+
+        return lo;
+    }
+
+    // Searches an element key in a pivoted
+    // sorted array arr of size n
+    static int search(int[] arr, int key) {
+        int n = arr.length;
+        int pivot = findPivot(arr, 0, n - 1);
+
+        // If we found a pivot, then first compare with pivot
+        // and then search in two subarrays around pivot
+        if (arr[pivot] == key)
+            return pivot;
+      
+        // If the minimum element is present at index
+        // 0, then the whole array is sorted
+        if (pivot == 0)
+            return binarySearch(arr, 0, n - 1, key);
+
+        if (arr[0] <= key)
+            return binarySearch(arr, 0, pivot - 1, key);
+        return binarySearch(arr, pivot + 1, n - 1, key);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+        int key = 3;
+        System.out.println(search(arr, key));
     }
 }
