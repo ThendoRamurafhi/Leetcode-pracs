76fa4f764a0639b4afc49d8aaf0f6688e9036874
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-09-22 17:10:58 +0200

    created a binary search file

---
+++ BinarySearch.java
@@ -0,0 +1,12 @@
+public class BinarySearch {
+    public static int binaryS
+
+    while(lo<=hi){
+            int mid = lo + (hi - lo)/2;
+            
+        }return-1;
+    
+    
+    
+    }
+}
