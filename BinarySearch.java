ee0923f296fd3ceca53acfaa9165e9aac09fa4f9
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-09-22 17:14:11 +0200

    binary search method/algorithm

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,12 +1,12 @@
 public class BinarySearch {
-    public static int binaryS
+    public static int binarySearch(int[] arr, int lo, int hi, int x){
 
-    while(lo<=hi){
+        while(lo<=hi){
             int mid = lo + (hi - lo)/2;
+            if(arr[mid] == x) return mid;
+            if(arr[mid] < x) lo = mid + 1;
+            else hi = mid - 1;
             
         }return-1;
-    
-    
-    
     }
 }
