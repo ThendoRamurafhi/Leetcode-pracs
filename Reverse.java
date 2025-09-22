a8a4d9ea4123ec097412a805f49af7ec7195367a
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-09-23 00:06:53 +0200

    created a reverse file that will reverse the integers given as input

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+public class Reverse {
+    public int reverse(int x) {
+        long rev = 0; // use long to handle overflow while computing
+        while (x != 0) {
+            int digit = x % 10;   // extract last digit
+            rev = rev * 10 + digit;
+            x /= 10; // remove last digit
+        }
+        // check overflow
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) rev;
+    }
+}
