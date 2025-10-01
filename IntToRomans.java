58c88b6e881b5826a3832b0777af39661a1d2126
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2025-10-01 23:26:03 +0200

    created a file that change integers to roman figures

---
+++ IntToRomans.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class IntToRomans {
+    public String intToRoman(int num) {
+        // LinkedHashMap preserves insertion order
+        LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            while (num >= entry.getKey()) {
+                sb.append(entry.getValue());
+                num -= entry.getKey();
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        IntToRoman converter = new IntToRoman();
+        System.out.println(converter.intToRoman(3749)); // MMMDCCXLIX
+        System.out.println(converter.intToRoman(58)); // LVIII
+        System.out.println(converter.intToRoman(1994)); // MCMXCIV
+    }
+}
